b66a172a3e0b651528a350bff33cedeb9c683802
jurgendn <nguyentiendung7299@gmail.com>
2020-12-15 18:43:18 +0700

    Neo4j post

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -10,3 +10,12 @@ if ($('.clean-product').length > 0) {
     });
 }
 
+$('.tofigure').each(function() {
+    $(this).replaceWith($('<figure class="img-with-caption tofigure">' + this.innerHTML + '</figure>'));
+});
+$('.tofigure').children('img').each(function() {
+    var caption;
+    caption = $(this).attr('title');
+    $(this).after('<figcaption class="caption">' + caption + '</figcaption>');
+});
+
